860f080e98969aad1cd96fba5309cf21c64dc36d
suaybkiraci <suayb2003@icloud.com>
2025-11-26 13:12:22 +0300

    fix

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import type { AdminUser, User } from '@/types';
-const API_URL = 'https://suayb.xyz';
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 const APPROVE_SEGMENT = 'approve';
 const UNAPPROVE_SEGMENT = 'unapprove';
 
